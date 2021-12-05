490a4c157e5695b87ce4dc5fdb2c1e09390835bf
Marius Kavoliunas <kavoliunas.m@gmail.com>
2021-12-05 21:07:04 +0200

    solve puzzle 4 part 1

---
+++ src/main/scala/BingoGame.scala
@@ -0,0 +1,58 @@
+package lt.markvl.adventofcode
+
+import monocle.syntax.all.*
+
+import scala.annotation.tailrec
+
+object BingoGame:
+
+  @tailrec
+  def firstWinBoardScore(choices: List[Int], boards: List[Board]): Int =
+    boards.find(_.isVictorious) match
+      case Some(board) => board.score
+      case None => firstWinBoardScore(choices.tail, boards.mark(choices.head))
+
+case class Board(lastCalledNumber: Option[Int], numbers: List[List[Number]])
+
+sealed trait Number
+
+case class Fresh(x: Int) extends Number
+case class Marked(x: Int) extends Number
+
+extension (board: Board)
+  def rows: List[List[Number]] = board.numbers
+  def columns: List[List[Number]] =
+    List
+      .fill(board.numbers.size)(board.numbers)
+      .zipWithIndex
+      .map((tbl, i) => tbl.map(_(i)))
+  def isVictorious: Boolean =
+    rows.hasFilled || columns.hasFilled
+  def score: Int =
+    board.lastCalledNumber
+      .map(_ * board.numbers.flatten.map {
+        case Fresh(x) => x
+        case _        => 0
+      }.sum)
+      .getOrElse(0)
+
+extension (numbers: List[List[Number]])
+  def hasFilled: Boolean = numbers.exists(_.forall {
+    case _: Marked => true
+    case _         => false
+  })
+
+extension (boards: List[Board])
+  def mark(number: Int): List[Board] =
+    boards.map(_.mark(number))
+
+extension (board: Board)
+  def mark(number: Int): Board =
+    board
+      .focus(_.lastCalledNumber)
+      .replace(Some(number))
+      .focus(_.numbers)
+      .modify(_.map(_.map {
+        case Fresh(x) if x == number => Marked(number)
+        case other                   => other
+      }))
---
+++ src/test/scala/PuzzleFourTest.scala
@@ -0,0 +1,45 @@
+package lt.markvl.adventofcode
+
+import monocle.syntax.all.*
+import utils.TestUtils
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class PuzzleFourTest extends AnyFlatSpec, Matchers, TestUtils:
+
+  def boardReader(
+      in: List[List[String]],
+      out: List[Board] = Nil,
+      acc: Board = Board(None, List.empty)
+  ): List[Board] =
+    in match
+      case Nil         => out :+ acc
+      case Nil :: tail => boardReader(tail, out :+ acc)
+      case head :: tail =>
+        boardReader(
+          tail,
+          out,
+          acc
+            .focus(_.numbers)
+            .modify(_ :+ head.map(n => Fresh.apply(n.toInt)))
+        )
+
+  def readData(path: String): (List[Int], List[Board]) =
+    val input =
+      path.readLines(
+        _.split(" +").filterNot(_.isBlank).toList
+      )
+    val choices = input.head.head.split(",").map(_.toInt).toList
+    val boards = boardReader(input.tail.tail)
+    choices -> boards
+
+  "Part One" should "work with example data" in {
+    val (choices, boards) = readData("/PuzzleFour/exampleInput")
+    BingoGame.firstWinBoardScore(choices, boards) should be(4512)
+  }
+
+  it should "Work with real data" in {
+    val (choices, boards) = readData("/PuzzleFour/input")
+    BingoGame.firstWinBoardScore(choices, boards) should be(27027)
+  }
