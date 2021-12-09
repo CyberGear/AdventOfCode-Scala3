508aab572546dd682dd3c74fba914d4ce7c0e346
Marius Kavoliūnas <marius.kavoliunas@telesoftas.com>
2021-12-09 17:21:00 +0200

    in progress to puzzle 6 part 2 solution

--- src/main/scala/Lanternfish.scala
+++ src/main/scala/Lanternfish.scala
@@ -2,15 +2,41 @@ package lt.markvl.adventofcode
 
 import scala.annotation.tailrec
 import scala.collection.IterableOnce
-
-case class Lanternfish(internalTimer: Long)
+import scala.collection.parallel.CollectionConverters.seqIsParallelizable
+import scala.collection.parallel.immutable.*
+import scala.runtime.LazyChar
 
 object Lanternfish:
+  opaque type Lanternfish = Short
+  def fromString(s: String): Lanternfish = s.toShort
+
   implicit val flatenner: Lanternfish => List[Lanternfish] = _ match
-    case Lanternfish(0)   => List(Lanternfish(6), Lanternfish(8))
-    case Lanternfish(int) => List(Lanternfish(int - 1))
+    case 0: Lanternfish => List(6.toShort, 8.toShort)
+    case x: Lanternfish => List((x - 1).toShort)
+
+  def schoolSize(school: List[Lanternfish], cycles: Int): Long =
+    dividedSchoolSize(ParSeq(school: _*), cycles)
 
   @tailrec
-  def schoolSize(school: List[Lanternfish], cycles: Int): Long =
-    if cycles == 0 then school.size
-    else schoolSize(school.flatten, cycles - 1)
+  def dividedSchoolSize(
+      school: ParSeq[Lanternfish],
+      cycles: Int,
+      queue: ParSeq[Segment] = ParSeq(),
+      acc: Long = 0
+  ): Long =
+    def log(s: String): Unit =
+      println(
+        s"$s school: ${school.size}; cycles: $cycles; queue: ${queue.size}; acc: $acc; "
+      )
+
+    if cycles == 0 && queue.isEmpty then acc + school.size
+    else if school.size > 10000000 then
+      log("/\\")
+      val (a, b) = school.splitAt(school.size / 2)
+      dividedSchoolSize(a, cycles, queue :+ Segment(b, cycles), acc)
+    else if cycles == 0 then
+      log("\\/")
+      dividedSchoolSize(queue.head.school, queue.head.cycles, queue.tail, acc + school.size)
+    else dividedSchoolSize(school.flatten, cycles - 1, queue, acc)
+
+case class Segment(school: ParSeq[Short], cycles: Int)
--- src/test/scala/PuzzleSixTest.scala
+++ src/test/scala/PuzzleSixTest.scala
@@ -5,6 +5,7 @@ import utils.TestUtils
 import monocle.syntax.all.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import Lanternfish.*
 
 class PuzzleSixTest extends AnyFlatSpec, Matchers, TestUtils:
 
@@ -13,7 +14,7 @@ class PuzzleSixTest extends AnyFlatSpec, Matchers, TestUtils:
   "Part One" should "work with example data" in {
     val lanternfish: List[Lanternfish] =
       "/PuzzleSix/exampleInput"
-        .readLines(_.split(",").map(data => Lanternfish(data.toLong)))
+        .readLines(_.split(",").map(Lanternfish.fromString))
         .head
         .toList
 
@@ -23,21 +24,21 @@ class PuzzleSixTest extends AnyFlatSpec, Matchers, TestUtils:
   it should "work with real data" in {
     val lanternfish: List[Lanternfish] =
       "/PuzzleSix/input"
-        .readLines(_.split(",").map(data => Lanternfish(data.toLong)))
+        .readLines(_.split(",").map(Lanternfish.fromString))
         .head
         .toList
 
     Lanternfish.schoolSize(lanternfish, 80) should be(352195)
   }
-//
-//  "Part two" should "work with example data" in {
-//    val lanternfish: List[Lanternfish] =
-//      "/PuzzleSix/exampleInput"
-//        .readLines(_.split(",").map(data => Lanternfish(data.toLong)))
-//        .head
-//        .toList
-//
-//    Lanternfish.schoolSize(lanternfish, 256) should be(26984457539L)
-//  }
-//
+
+  "Part two" should "work with example data" in {
+    val lanternfish: List[Lanternfish] =
+      "/PuzzleSix/exampleInput"
+        .readLines(_.split(",").map(Lanternfish.fromString))
+        .head
+        .toList
+
+    Lanternfish.schoolSize(lanternfish, 256) should be(26984457539L)
+  }
+
 //  it should "work with real data" in {}
